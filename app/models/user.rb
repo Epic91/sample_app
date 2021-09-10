febd3bcdc819ef8badc43fdb19264840a072e0b4
Epic91 <hernandeze791@gmail.com>
2021-09-10 15:32:41 -0500

    changed before save method, added to email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-    before_save { self.email = email.downcase }
+    before_save { email.downcase! }
 
     has_secure_password
     validates :password, length: { minimum: 6 }
 
     validates :name, presence: true, length: { maximum: 50 }
     # ensures that only email addresses that match the pattern are considered valid
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, 
     # Rails infers that uniqueness should be true as well.
     uniqueness: { case_sensitive: false }
