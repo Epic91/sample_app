888c16c6e2bc4fc4aea7967dc8c3edb4a14d6bc7
Epic91 <hernandeze791@gmail.com>
2021-09-14 13:16:13 -0500

    added to test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,14 +16,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty? #verify that the flash message doesnt appear on the new page
   end
 
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get login_path #visit the login path
     post login_path, session: { email: @user.email, password: 'password' }
+    assert is_logged_in?
     assert_redirected_to @user # check for the correct redirect target
     follow_redirect! # after checking for the redirect target actually visit the target page
     assert_template 'users/show' #verify that the users show form renders properly
     assert_select "a[href=?]", login_path, count: 0 # verifies that the login link disappears by verifying that there are zero login path links on the page
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
-  end
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path 
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    end
 end
