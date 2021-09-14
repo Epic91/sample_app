534c1a7b0fb1dd567263a0000a855973d44240e2
Epic91 <hernandeze791@gmail.com>
2021-09-14 12:56:15 -0500

    new method and test for valid info

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+  
   test "login with invalid information" do
     get login_path #visit the login path
     assert_template 'sessions/new' #verify that the new sessions form renders properly
@@ -9,5 +14,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path #visit another page
     assert flash.empty? #verify that the flash message doesnt appear on the new page
+  end
+
+  test "login with valid information" do
+    get login_path #visit the login path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user # check for the correct redirect target
+    follow_redirect! # after checking for the redirect target actually visit the target page
+    assert_template 'users/show' #verify that the users show form renders properly
+    assert_select "a[href=?]", login_path, count: 0 # verifies that the login link disappears by verifying that there are zero login path links on the page
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
