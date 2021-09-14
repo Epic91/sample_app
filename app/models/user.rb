8d95777ef070fbdbf2d6a04f5ba3a4dec54d6f77
Epic91 <hernandeze791@gmail.com>
2021-09-14 12:54:35 -0500

    method to hash password

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,11 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, 
     # Rails infers that uniqueness should be true as well.
     uniqueness: { case_sensitive: false }
+
+    #returns the hash digest of the given string
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+      end
 end
